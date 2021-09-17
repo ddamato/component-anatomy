e6e06af628606cba74719fbf599fdc3c405a40e9
Donnie D'Amato <fauxserious@gmail.com>
2021-09-17 07:21:05 -0400

    Add test for individual links

--- test/index.spec.js
+++ test/index.spec.js
@@ -100,6 +100,17 @@ describe('<component-anatomy/>', function () {
     expect(item.innerHTML).to.equal('Button text with <a href="#link">a link</a> to a place');
   });
 
+  it('should create only link', function () {
+    const term = '[a link](#link)';
+    anatomy.create({ x:'64%', y:'53%', term });
+    const item = anatomy.shadowRoot.querySelector('#list li');
+    const link = anatomy.shadowRoot.querySelector('#list li a');
+    expect(link).to.exist;
+    expect(link.getAttribute('href')).to.equal('#link');
+    expect(link.textContent).to.equal('a link');
+    expect(item.innerHTML).to.equal('<a href="#link">a link</a>');
+  });
+
   it('should not render html', function () {
     const term = 'Button text with <strong>bold</strong> wording';
     anatomy.create({ x:'64%', y:'53%', term });
